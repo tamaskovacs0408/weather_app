4326adce55dad9ee278cf8054942bacf8b6b7ed0
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-28 08:39:24 +0200

    Styling searchbar

--- src/Component/Search/Search.js
+++ src/Component/Search/Search.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { AsyncPaginate } from 'react-select-async-paginate';
 import { GEO_API_URL, geoApiOptions } from '../api';
+import "./Search.css";
 
 const Search = ({onSearchChange}) => {
 
@@ -29,6 +30,7 @@ const Search = ({onSearchChange}) => {
 
   return (
     <AsyncPaginate 
+      className='searchbar'
       placeholder='Search for city'
       debounceTimeout={600}
       value={search}
